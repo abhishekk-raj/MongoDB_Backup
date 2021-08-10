7b50050671e78136e49c66fd52f53bc0d3772203
Abhishek Raj <abhishek@betatestsolutions.com>
2021-08-10 21:26:16 +0530

    Added cron job

--- src/mongo-backup.ts
+++ src/mongo-backup.ts
@@ -1,1 +1,25 @@
-console.log('running successfully...');
+import {CronJob} from 'cron';
+
+class MongoBkp {
+
+    cronJob: CronJob;
+
+    public takeMongoBackup(): void {
+        this.cronJob = new CronJob('* * * * * *', async () => {
+            try {
+                console.log('Running...');
+            } catch (e) {
+                console.error(e);
+            }
+        });
+
+        // Start job
+        if (!this.cronJob.running) {
+            this.cronJob.start();
+        }
+        console.log('Started App...');
+    }
+}
+
+const mongoBkp = new MongoBkp();
+mongoBkp.takeMongoBackup();
